543ed08a1ba4791208260d1c1a8682af1f7ff1d6
savanmyndro <savan@myndro.com>
2021-10-01 12:02:35 +0530

    cell

---
+++ gity/TableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.swift
+//  gity
+//
+//  Created by admin on 01/10/21.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
